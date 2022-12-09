84be12bbeb30b057681db6eae5391b2255d1a3f0
virendraepresence <114769583+virendraepresence@users.noreply.github.com>
2022-12-09 23:37:15 +0530

    Add files via upload

---
+++ language.js
@@ -0,0 +1,133 @@
+function changeBackgroundColourWithID(){
+    
+    const paragraph = document.getElementById("para");
+    paragraph.style.color = "white";
+    paragraph.style.backgroundColor = "#368BC1";
+    paragraph.style.padding = "20px";
+    console.log(paragraph);
+}
+
+changeBackgroundColourWithID();
+
+// function changeBackgroundColourWithID(){
+//     // Document Object Contains Many Predefined Funtions0
+//     // It will return the element that has ID "para"
+//     const paragraph = document.getElementById("para");
+    
+//     console.log(paragraph);
+// }
+
+
+
+// changeBackgroundColourWithID();
+
+// function changeBackgroundColourWithClassName(){
+//     const para = document.getElementsByClassName("para");
+//     console.log("para name", para);
+// }
+
+// changeBackgroundColourWithClassName();
+
+// function changeBackgroundColourWithTagName(){
+//     const para = document.getElementsByTagName("p");
+//     console.log("para name", para);
+// }
+
+// changeBackgroundColourWithTagName();
+
+// function changeBackgroundColourWithQuerySelector(){
+//     // QuerySelector only returns the first element that matches the criteria
+//     const para = document.querySelector(".ps");
+//     console.log("para name", para);
+// }
+
+// changeBackgroundColourWithQuerySelector();
+
+
+// function changeBackgroundColourWithQuerySelectorAll(){
+    
+//     // QuerySelector returns all the elements that matches the criteria
+//     const para = document.querySelectorAll(".ps");
+//     console.log("para name", para);
+// }
+
+// changeBackgroundColourWithQuerySelectorAll();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getName();
+// console.log(a);
+// var a = 7;
+// function getName(){
+//     console.log("My name is abcd");
+// }
+
+// var getNameDuplicate=()=>{
+//     //function body
+// }
+
+// getName();
+// console.log(getName);
+// var getName = function(){
+//     console.log("Juno batch");
+// }
+
+
+// console.log("Value of X is", x);
+// var x = 7;
+
+// // varible exists in the memory but not have a value
+// try{
+//     console.log("Value of Y is", y);
+// }
+// catch(err){
+//     console.log("There is an erro", err);
+// }
+
+
+// function sayMyName(name){
+//     console.log("my name is ", name);
+// }
+
+// sayMyName();
+
+//reference error occurs and stops the execution of the
+// program any further when variable is not present
+// in the memory
